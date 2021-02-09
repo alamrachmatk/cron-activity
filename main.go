14c809cb355d87e28d92cdd87c0ee97141d18c40
alam.kurniawan <alam.kurniawan@mncgroup.com>
2021-02-09 21:49:20 +0700

    a

--- main.go
+++ main.go
@@ -52,16 +52,16 @@ func main() {
 	dbConn := initDB()
 	redisConn := initRedis()
 
-	gocron.Every(1).Day().At("21:15").Do(controllers.TotalDns, dbConn, redisConn)
-	gocron.Every(1).Day().At("21:15").Do(controllers.TotalBlock, dbConn, redisConn)
-	gocron.Every(1).Day().At("21:15").Do(controllers.TotalDnsBlock, dbConn, redisConn)
-	gocron.Every(1).Day().At("21:15").Do(controllers.TotalIpAdress, dbConn, redisConn)
-	gocron.Every(1).Day().At("21:15").Do(controllers.TotalMostActiveList, dbConn, redisConn)
-	gocron.Every(1).Day().At("21:15").Do(controllers.TotalDnsDayList, dbConn, redisConn)
-	gocron.Every(1).Day().At("21:15").Do(controllers.TotalIpAddressDayList, dbConn, redisConn)
-	gocron.Every(1).Day().At("21:15").Do(controllers.BlockList, dbConn, redisConn) //harus di server karna bakal banyak data list block
-	gocron.Every(1).Day().At("21:15").Do(controllers.TotalIpAddressBlockCategoryDayList, dbConn, redisConn)
-	gocron.Every(1).Day().At("21:15").Do(controllers.TotalDnsBlockCategoryDayList, dbConn, redisConn)
+	gocron.Every(1).Day().At("21:55").Do(controllers.TotalDns, dbConn, redisConn)
+	gocron.Every(1).Day().At("21:55").Do(controllers.TotalBlock, dbConn, redisConn)
+	gocron.Every(1).Day().At("21:55").Do(controllers.TotalDnsBlock, dbConn, redisConn)
+	gocron.Every(1).Day().At("21:55").Do(controllers.TotalIpAdress, dbConn, redisConn)
+	gocron.Every(1).Day().At("21:55").Do(controllers.TotalMostActiveList, dbConn, redisConn)
+	gocron.Every(1).Day().At("21:55").Do(controllers.TotalDnsDayList, dbConn, redisConn)
+	gocron.Every(1).Day().At("21:55").Do(controllers.TotalIpAddressDayList, dbConn, redisConn)
+	gocron.Every(1).Day().At("21:55").Do(controllers.BlockList, dbConn, redisConn) //harus di server karna bakal banyak data list block
+	gocron.Every(1).Day().At("21:55").Do(controllers.TotalIpAddressBlockCategoryDayList, dbConn, redisConn)
+	gocron.Every(1).Day().At("21:55").Do(controllers.TotalDnsBlockCategoryDayList, dbConn, redisConn)
 
 	<-gocron.Start()
 }
